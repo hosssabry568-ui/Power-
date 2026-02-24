09edea8d9e1334962434963bf063a79be644314d
hosssabry568-ui <hosssabry568@gmail.com>
2026-02-24 14:32:03 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,21 @@
-const CACHE_NAME = 'alnoor-cache-v1';
+const CACHE_NAME = "alnoor-cache-v2";
+
 const assets = [
-  'index.html',
-  'quran-simple.xml',
-  'manifest.json',
-  'https://fonts.googleapis.com/css2?family=Amiri:wght@400;700&family=Noto+Naskh+Arabic:wght@400;700&family=Reem+Kufi:wght@400;700&display=swap',
-  'https://www.transparenttextures.com/patterns/islamic-art.png'
+  "./",
+  "./index.html",
+  "./quran-simple.xml",
+  "./icon.png"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
